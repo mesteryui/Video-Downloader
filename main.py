d1cb79db83930a95b950c9428cc147fd199c1d66
mesteryui <108957859+mesteryui@users.noreply.github.com>
2024-10-02 09:16:50 +0200

    Update main.py
    Simplificacion a pesar del no funcionamiento

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3 
-from gi.repository import GLib
-from pathlib import Path
+
 from pytube import YouTube
 
 def main():
-    destination = Path(GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOWNLOAD))
+    destination = "Descargas/" 
     link = input("Enter the link: ")
     yt = YouTube(link)
     ys = yt.streams.get_highest_resolution()
